ad68c7d42a1a0fad366a13fe5602074b49459bf8
hqy <3539761344@qq.com>
2019-06-21 19:10:15 +0800

    把项目添加到本地仓库

---
+++ src/test03/Demo01.java
@@ -0,0 +1,27 @@
+package test03;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Demo01 {
+    public static void main(String[] args) throws ParseException {
+        demo();
+        demo1();
+
+    }
+
+    private static void demo1() throws ParseException {
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy年MM月dd号 HH:mm:ss");
+        Date p = sdf.parse("2019年03月23号 15:15:27");
+        System.out.println(p);
+    }
+
+    private static void demo() {
+        Date d=new Date();
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy年MM月dd号 HH:mm:ss");
+        String format = sdf.format(d);
+        System.out.println(format);
+    }
+
+}
